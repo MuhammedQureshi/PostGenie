9559ee721018964563be59ba0f1fdd2f29e6b3af
LilAliTech <lilalix20@gmail.com>
2024-02-11 14:35:59 +0000

    Add clerk auth

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className='relative isolate pt-[6rem] dark:bg-gray-900'>
         <div className='absolute inset-x-0 top-28 -z-10 transform-gpu overflow-hidden blur-3xl' aria-hidden="true">
           <div
-            className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875]'
+            className='relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875]'
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
@@ -23,9 +23,9 @@ export default function Home() {
               Post Genie
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
-              You speak your language, they speak their language. {""}
+              Make your tweets more interesting {""}
               <span className="Otext-indigo-600 dark: text-indigo-500">
-              Let AI handle the translation.
+              Let AI handle the transformation.
               </span>
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,15 @@
+import { UserButton, auth } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
+
+  const {userId} = auth();
+
   return (
     <div className='z-50 fixed top-8 left-1/2 transform -translate-x-1/2 inline-flex mx-auto justify-between w-11/12 sm:w-1/2 rounded-full backdrop-blur-xl border border-1 border-purple-500/10 backdrop-filter bg-purple-900/10 px-6 py-2 items-center'>
-        <Link href={'/'} className='flex justify-between items-center'>
+        <Link href={'/'} className='flex justify-between items-center select-none'>
             <Image src="/Logo.png" alt="logo" className='' width={54} height={30}/>
             <h1 className=' text-2xl font-sans'>ostGenie</h1>
         </Link>
@@ -15,6 +19,16 @@ const Navbar = () => {
                Pricing
             </Link>
             {/* Login button/User Avatar using clerk */}
+            { userId ? 
+              <UserButton /> 
+              :
+             (
+              <Link href='/sign-in' className=' border p-2 px-4 rounded-xl'>
+                SignIn
+              </Link>
+             )
+            }
+            
         </div>
     </div>
   )
