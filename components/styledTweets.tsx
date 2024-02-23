bcf1f5995f800a1dd4f94d7b166d36821bb8f5b1
LilAliTech <lilalix20@gmail.com>
2024-02-23 20:13:20 +0000

    add tweetStyle badge

--- components/styledTweets.tsx
+++ components/styledTweets.tsx
@@ -1,23 +1,27 @@
 import { Button } from "@/components/ui/button"
-import { CardContent, Card, CardTitle } from "@/components/ui/card"
-import { CardHeader } from "@nextui-org/react"
+import { Badge } from "@/components/ui/badge"
+import { CardContent, Card} from "@/components/ui/card"
 import { CopyIcon } from "lucide-react"
 
 
 const StyledTweets = () => {
   return (
-    <Card className="sm:mx-20 mt-20 flex justify-center items-center">
-    <CardContent className="flex p-4 gap-6 items-center w-full justify-between">
-      <div className="flex flex-col">
-        <p>I just saw a guy at the gym put a water bottle in the Pringles holder on the treadmill.</p>
-        <div>funny</div>
-      </div>
-      <Button className="flex items-center justify-end" variant="outline">
-        <CopyIcon className="w-4 h-4" />
-        <span className="sr-only">Copy</span>
-      </Button>
-    </CardContent>
-  </Card>
+    <div>
+      <Card className="sm:mx-20 mt-20 flex justify-center">
+        <CardContent className="flex p-4 gap-6 w-full justify-between">
+          <div className="flex flex-col">
+            <p>I just saw a guy at the gym put a water bottle in the Pringles holder on the treadmill.</p>
+            <div className="pt-2">
+            <Badge variant="outline">😂 Funny</Badge>
+            </div>
+          </div>
+          <Button className="flex items-center justify-end" variant="outline">
+            <CopyIcon className="w-4 h-4" />
+            <span className="sr-only">Copy</span>
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
 
